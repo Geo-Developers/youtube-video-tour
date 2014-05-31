20ce4dd9af3bb929f4ee265e55dedb5b211b5bb9
RaulJimenezOrtega <hhkaos@gmail.com>
2014-05-31 16:57:52 +0200

    Cleaning files

---
+++ config/config.js
@@ -0,0 +1,15 @@
+{
+    "configurationSettings": [
+        {
+            "category": "Settings",
+            "fields": [{
+		        "type": "string",
+		        "fieldName": "description",
+		        "label": "Youtube ID",
+		        "tooltip": "",
+		         "stringFieldOption": "textbox",
+		        "placeHolder": "Ex. PQuWhPpH3Eo"
+			}]
+        }
+    ]
+}
